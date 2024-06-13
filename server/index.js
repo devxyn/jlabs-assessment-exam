5f39c853a8ad4688a1a014436ab7b36a9c432172
devxyn <johnlawrencehermo@gmail.com>
2024-06-13 12:30:25 +0800

    feat: connected to the database

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import 'dotenv/config.js';
-
 import { authRoutes } from './src/routes/authRoutes.js';
 
 const app = express();
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 8000;
+
+try {
+  await mongoose.connect('mongodb://localhost:27017', {
+    dbName: 'JLabs-Exam',
+  });
+  console.log('Connected to the database!');
+} catch (error) {
+  console.error('Database connection error!', error.message);
+}
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api', authRoutes);
 
-app.listen(() => console.log(`Server is running in port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
