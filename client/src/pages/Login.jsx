988168e3bddf8e443a7d41cf996fab6ddb3d609b
devxyn <johnlawrencehermo@gmail.com>
2024-06-13 17:17:06 +0800

    feat: added styling in login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const [_, setCookies] = useCookies('access_token');
   const navigate = useNavigate();
 
-  const messageBg = isSuccess ? { backgroundColor: 'lightgreen' } : { backgroundColor: 'lightcoral' };
+  const messageBg = isSuccess ? 'bg-green-500/50' : 'bg-red-500/50';
 
   const handleChange = (e) => {
     setLoginInfo({ ...loginInfo, [e.target.name]: e.target.value });
@@ -35,15 +35,31 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form method='POST' onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='email'>Email:</label>
-          <input type='email' name='email' id='email' value={loginInfo.email} onChange={handleChange} required />
+    <div className='w-full h-screen bg-background text-white flex flex-col justify-center items-center'>
+      <form
+        className='bg-primary p-10 flex flex-col justify-center items-center gap-2 w-full md:w-1/3 lg:gap-5'
+        method='POST'
+        onSubmit={handleSubmit}>
+        <h1 className='text-3xl font-bold lg:text-4xl'>Login</h1>
+
+        <div className='relative w-full'>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            value={loginInfo.email}
+            onChange={handleChange}
+            required
+            className='block px-2.5 pb-2.5 pt-5 w-full text-sm text-white bg-secondary rounded-lg border border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer'
+            placeholder=' '
+          />
+          <label
+            htmlFor='email'
+            className='absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-5 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-4 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1'>
+            Email
+          </label>
         </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
+        <div className='relative w-full'>
           <input
             type='password'
             name='password'
@@ -51,10 +67,18 @@ const Login = () => {
             value={loginInfo.password}
             onChange={handleChange}
             required
+            className='block px-2.5 pb-2.5 pt-5 w-full text-sm text-white bg-secondary rounded-lg border border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer'
+            placeholder=' '
           />
+          <label
+            htmlFor='password'
+            className='absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-5 z-10 origin-[0] bg-transparent px-2 peer-focus:px-2 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-4 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1'>
+            Password
+          </label>
         </div>
-        {message && <p style={messageBg}>{message}</p>}
-        <button type='submit' disabled={isSuccess}>
+
+        {message && <p className={`w-full p-2 text-sm px-4 rounded-lg border ${messageBg}`}>{message}</p>}
+        <button className='px-4 py-2 bg-secondary rounded-lg' type='submit' disabled={isSuccess}>
           Login
         </button>
       </form>
